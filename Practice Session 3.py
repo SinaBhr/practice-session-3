4d6213b339b85be29c0f2416419411e27207a285
Sina Bahraminejad <125271549+SinaBhr@users.noreply.github.com>
2023-02-15 16:27:10 +0330

    Add files via upload

---
+++ Practice Session 3.py
@@ -0,0 +1,18 @@
+#%% practice
+
+num1="74.250"
+num2="13"
+
+num1=float(num1)
+num2=int(num2)
+
+num3=num1/num2
+num3=round(num3,2)
+print(num3)
+
+num4=num1//num2
+print(num4)
+
+num5=num1%num2
+print(num5)
+
